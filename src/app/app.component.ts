918cb83e87d237741edf72399abf6369d955a527
Tanja <tanja120a@gmail.com>
2021-04-03 10:26:01 +0200

    generate projects component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'akvelon-angular-project';
+  title: string= 'Akvelon Angular Project';
 }
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../project';
+import { PROJECTS } from '../mock-projects';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+    title: string = 'Projects';
+    projects: Project[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(){
+    this.projects = PROJECTS;
+  }
+
+}
