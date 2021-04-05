b1f1e653d01d939d17dffab859016117be886d22
Tanja <tanja120a@gmail.com>
2021-04-05 11:45:19 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MessagesComponent } from './messages/messages.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { ProjectEditComponent } from './project-edit/project-edit.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { TaskDetailComponent } from './task-detail/task-detail.component';
     DashboardComponent,
     MessagesComponent,
     TasksComponent,
-    TaskDetailComponent
+    TaskDetailComponent,
+    ProjectEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/project-edit/project-edit.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { ProjectService } from '../project.service';
+
+import { Project } from '../project';
+
+@Component({
+  selector: 'app-project-edit',
+  templateUrl: './project-edit.component.html',
+  styleUrls: ['./project-edit.component.css']
+})
+export class ProjectEditComponent implements OnInit {
+
+  title: string = "Add a new project";
+  projects: Project[] = [];
+  addedProject: Project = new Project();
+  @Output()
+  addedProjectEvent = new EventEmitter();
+
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(): void{
+    this.projectService.getProjects()
+        .subscribe((data) => this.projects = data);
+  }
+
+  add(): void {
+    //name = name.trim();
+   //const tempProject = new Project();
+   //this.selectedProject.name = name;
+   this.addedProject.date = new Date().toUTCString();
+    if (!this.addedProject.name) { return; }
+    this.projectService.addProject(this.addedProject)
+      .subscribe(project => {
+        this.projects.push(project);
+        this.addedProjectEvent.emit();
+      });
+
+  }
+
+}
